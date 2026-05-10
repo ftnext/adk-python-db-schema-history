8be8ad12672771329f906a942895d89c03af0537
ftnext <takuyafjp+develop@gmail.com>
2026-05-10 14:55:03 +0900

    chore(nix): multi systems

--- flake.nix
+++ flake.nix
@@ -10,42 +10,54 @@
       ...
     }:
     let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.docker
-          pkgs.git
-          pkgs.libpq
-          pkgs.mysql84
-          pkgs.postgresql
-          pkgs.python313
-          pkgs.uv
-          pkgs.sqldef
-          pkgs.curl
-          pkgs.lsof
-          pkgs.coreutils
-          pkgs.bash
-          (pkgs.writeShellScriptBin "pg_config" ''
-            case "''${1:-}" in
-              --libdir)
-                echo "${pkgs.libpq}/lib"
-                ;;
-              --version)
-                echo "PostgreSQL ${pkgs.libpq.version}"
-                ;;
-              *)
-                echo "unsupported pg_config argument: ''${1:-}" >&2
-                exit 1
-                ;;
-            esac
-          '')
-        ];
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.docker
+              pkgs.git
+              pkgs.libpq
+              pkgs.mysql84
+              pkgs.postgresql
+              pkgs.python313
+              pkgs.uv
+              pkgs.sqldef
+              pkgs.curl
+              pkgs.lsof
+              pkgs.coreutils
+              pkgs.bash
+              (pkgs.writeShellScriptBin "pg_config" ''
+                case "''${1:-}" in
+                  --libdir)
+                    echo "${pkgs.libpq}/lib"
+                    ;;
+                  --version)
+                    echo "PostgreSQL ${pkgs.libpq.version}"
+                    ;;
+                  *)
+                    echo "unsupported pg_config argument: ''${1:-}" >&2
+                    exit 1
+                    ;;
+                esac
+              '')
+            ];
 
-        shellHook = ''
-          export LIBPQ_DIR="${pkgs.libpq}"
-        '';
-      };
+            shellHook = ''
+              export LIBPQ_DIR="${pkgs.libpq}"
+            '';
+          };
+        }
+      );
     };
 }
